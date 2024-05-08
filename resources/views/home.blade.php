f22da3a781d4ec8dc8ae780bdb1c8b7480af29df
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-05-08 14:27:15 +0200

    [add] custom home page and [print] "Hello Laravel" in it

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    {{-- Bootstrap --}}
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css'
+        integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=='
+        crossorigin='anonymous' />
+
+    <title>Laravel Primi Passi</title>
+</head>
+
+<body>
+    <div class="container my-3">
+        <h1 class="text-center">Hello Laravel</h1>
+    </div>
+</body>
+
+</html>
