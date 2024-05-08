d9e064e0f5479a2fae7fe4707377b2e06db37c5b
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-05-08 15:21:03 +0200

    [refine] style and [fix] href

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,24 +11,21 @@
         integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=='
         crossorigin='anonymous' />
 
-    {{-- CSS --}}
-    <link rel="stylesheet" href="public/css">
-
     <title>Laravel Primi Passi</title>
 </head>
 
 <body>
     <header>
         <nav>
-            <ul class="list-unstyled d-flex justify-content-evenly bg-body-secondary py-3">
-                <li><a class="text-decoration-none" href="/">Home</a></li>
-                <li><a class="text-decoration-none" href="/about-Laravel">About</a></li>
-                <li><a class="text-decoration-none" href="/documentation-Laravel">Documentation</a></li>
+            <ul class="list-unstyled d-flex justify-content-evenly bg-danger py-4">
+                <li><a class="text-decoration-none text-white" href="{{ route('home') }}">Home</a></li>
+                <li><a class="text-decoration-none text-white" href="{{ route('about') }}">About</a></li>
+                <li><a class="text-decoration-none text-white" href="{{ route('documentation') }}">Documentation</a>
+                </li>
             </ul>
         </nav>
     </header>
     <div class="container my-5">
-        <h1 class="text-center">About Laravel</h1>
         <p>Laravel is a web application framework with expressive, elegant syntax. We have already laid the foundation —
             freeing you to create without sweating the small things.</p>
     </div>
